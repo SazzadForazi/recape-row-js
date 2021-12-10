abb03694971e92062ef5117029ba15149d982edc
Sazzad Hossain <sazzad.csebubt@gmail.com>
2021-12-10 23:25:00 +0600

    first commit

--- B_evenodd.js
+++ B_evenodd.js
@@ -1,7 +1,10 @@
 
 function oddEven(num1, num2) {
     for (var i = num1; i <= num2; i++) {
-        console.log(i);
+        if (i % 2 == 0) {
+            console.log('EVEN Number :', i);
+        }
+
     }
 }
 oddEven(7, 23)
